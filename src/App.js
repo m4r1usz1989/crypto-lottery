ba30799acb28e8e56a0d3c9871e47aaf371f389b
mariuszd <mariusz.domanski@galactica.pl>
2022-11-10 11:41:09 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMetaMask } from "metamask-react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
+
+  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+
+  if (status === "unavailable") return <div>MetaMask not available :(</div>
+
+  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+
+  if (status === "connecting") return <div>Connecting...</div>
+
+  if (status === "connected") return <div>Connected account {account} on chain ID {chainId}</div>
+
+  return null;
 }
 
 export default App;
